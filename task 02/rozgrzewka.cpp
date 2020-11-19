5ffd1f7166b394f537882f2108d6b87e3d874e3b
xwaruz1212 <kaczur190@gmail.com>
2020-11-19 16:35:28 +0100

    task rozgrzewka done

---
+++ task 02/rozgrzewka.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+int liczba;
+
+int main()
+{
+    cout<< "Witaj! Oto 10 losowych liczb z przedzia³u od 1-100: \n";
+    srand(time(NULL));
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+    liczba = rand()%100+1;
+    cout<< liczba<< endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
