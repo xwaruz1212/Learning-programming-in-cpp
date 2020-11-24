d4d4d91cf446f7f2f75c8b0f180f6807b9af6039
xwaruz1212 <kaczur190@gmail.com>
2020-11-24 21:10:23 +0100

    minikasyno

--- task 02/MiniKasyno/main.cpp
+++ task 02/MiniKasyno/main.cpp
@@ -16,12 +16,17 @@ int main()
     int (liczba_trafien = 0);
     srand(time(NULL));
 
-    cout << "Witaj w totolotku! Wpisz 4 liczby od 1-99: \a" << endl;
+    cout << "Witaj w MiniKasynie! Podaj 4 liczby w zakresie 0-1:" << endl;
 
     for(int i=0; i<4; i++)
     {
         cout<< " Podaj "  << i+1 <<  " liczbe : \n";
         cin>> zgadywane [i];
+        while (zgadywane [i] != (0|| 1))
+        {
+            cout<< "Liczba musi byc w przedziale 0-1...\n Sprobuj jeszcze raz: ";
+            cin>> zgadywane [i];
+        }
     }
 
 
@@ -31,7 +36,7 @@ int main()
     Sleep(3000);
     for (int i=0; i<4; i++)
     {
-        losowane [i]=rand()%100+1;
+        losowane [i]=rand()%1;
         cout<< losowane[i]<< endl;
 
         if (zgadywane[i] == losowane[i])
